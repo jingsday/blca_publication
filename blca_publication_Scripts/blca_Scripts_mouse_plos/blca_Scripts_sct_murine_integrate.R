5bc20b8b5d64634faadf5938f1049b0608ee3be7
jingsday <lidiasday@gmail.com>
2025-03-06 16:09:37 +0100

    biomart got less genes converted than before

--- blca_publication_Scripts/blca_Scripts_mouse_plos/blca_Scripts_sct_murine_integrate.R
+++ blca_publication_Scripts/blca_Scripts_mouse_plos/blca_Scripts_sct_murine_integrate.R
@@ -221,14 +221,3 @@ write.table(rownames(corrected_UMI), paste0(outdir, "blca_publication_OUTPUT_sct
 write.table(colnames(corrected_UMI), paste0(outdir,"blca_publication_OUTPUT_sct/sct_corrected_UMI_genes.txt"), col.names = FALSE, row.names = FALSE)
 
 
-
-colnames_substr <- substr(colnames(corrected_UMI), 1, 10)
-
-#input interested datasets
-indices <- which(colnames_substr == "GSM5288674")
-GSM5288674_corrected_UMI <- corrected_UMI[, indices]
-GSM5288674_corrected_UMI<- t(GSM5288674_corrected_UMI)
-writeMM(GSM5288674_corrected_UMI, "sct_full_UM/GSM5288674_corrected_UMI.mtx")
-write.table(rownames(GSM5288674_corrected_UMI), file = "sct_full_UM/GSM5288674_corrected_UMI_cells.txt", col.names = FALSE, row.names = FALSE)
-write.table(colnames(GSM5288674_corrected_UMI), file = "sct_full_UM/GSM5288674_corrected_UMI_genes.txt", col.names = FALSE, row.names = FALSE)
-
