b834f9f7eed501a8b064c525f7b97673b330ba62
jingsday <lidiasday@gmail.com>
2025-04-21 20:09:41 +0200

    new dataset

---
+++ blca_publication_Scripts/blca_Scripts_scRNA/blca_sc_ta.R
@@ -0,0 +1,17 @@
+library(Seurat)
+
+# For output from CellRanger >= 3.0 with multiple data types
+data_dir <- '/home/jing/Phd_project/project_UCD_blca/blca_DATA/blca_DATA_GSE135337/'
+list.files(data_dir) # Should show barcodes.tsv.gz, features.tsv.gz, and matrix.mtx.gz
+data <- Read10X(data.dir = data_dir,gene.column = 1,)
+
+
+seurat_object = CreateSeuratObject(counts = data$`Gene Expression`)
+seurat_object[['Protein']] = CreateAssayObject(counts = data$`Antibody Capture`)
+
+
+counts <- read.delim(paste0(data_dir,"GSM5329919_BCN_gene_cell_exprs_table.xls.gz"), row.names=1)
+
+str(counts)
+
+seurat_object = CreateSeuratObject(counts = counts)
