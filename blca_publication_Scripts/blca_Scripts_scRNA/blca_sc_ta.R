13b7a45477148cf225b841d8d68625a8ea8d89f0
jingsday <lidiasday@gmail.com>
2025-04-23 14:25:23 +0200

    .

--- blca_publication_Scripts/blca_Scripts_scRNA/blca_sc_ta.R
+++ blca_publication_Scripts/blca_Scripts_scRNA/blca_sc_ta.R
@@ -1,25 +1,43 @@
 library(Seurat)
-
+library(ggplot2)
+library(tidyverse)
+library(gridExtra)
+library(Matrix)
+library(stringr)
+library(sctransform)
 # For output from CellRanger >= 3.0 with multiple data types
 data_dir <- '/home/jing/Phd_project/project_UCD_blca/blca_DATA/blca_DATA_GSE135337/'
-list.files(data_dir) # Should show barcodes.tsv.gz, features.tsv.gz, and matrix.mtx.gz
-data <- Read10X(data.dir = data_dir,gene.column = 1,)
+dirs <- list.files(data_dir) # Should show barcodes.tsv.gz, features.tsv.gz, and matrix.mtx.gz
 
 
-seurat_object = CreateSeuratObject(counts = data$`Gene Expression`)
-seurat_object[['Protein']] = CreateAssayObject(counts = data$`Antibody Capture`)
+
+names_list <- c()  # Create an empty list to store the names
+for (x in dirs) {
+  name <- unique(str_sub(x, end = 14))  # Extract the name
+  names_list <- c(names_list, name)     # Append the name to the list
+}
+names_list <-unique(names_list)
+names_list
+
+names_list <- names_list[c(1,4,5,6,7)]
+
+#Select Ta low grade as they don't invade 
 
 
-counts <- read.delim(paste0(data_dir,"GSM5329919_BCN_gene_cell_exprs_table.xls.gz"), row.names=1)
+for (name in names_list) {
+  cts <- read.delim(paste0(data_dir,name, "_gene_cell_exprs_table.txt.gz"), row.names=1)  
+  # create seurat objects
+  assign(name, CreateSeuratObject(counts = cts))
+}
 
-str(counts)
+ls()
+#1,6,7  Ta  4,5 T2 and T3  
 
-seurat_object = CreateSeuratObject(counts = counts)
 
-str(seurat_object)
+merged_seurat <- merge(GSM4006644_BC1, y = c(GSM4006647_BC4,GSM4006648_BC5, GSM4751267_BC6, GSM4751268_BC7),
+                       add.cell.ids = ls()[3:9],
+                       project = 'BLCA')
 
-data <- read.delim("~/Desktop/GSE155512_RAW/GSM4705589_RPE004_matrix.txt.gz", header = T, stringsAsFactors = F)
-seurat <- CreateSeuratObject(data, min.cells = 10, min.features = 200, project = "seurat")
-seurat[["percent.mt"]] <- PercentageFeatureSet(seurat, pattern = "^MT-")
+
 
 #Ta vs MIBC  but need to read. Luminal T3, even mixed cell types in Ta
